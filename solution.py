982daa44099bbae36807f31ad0d494cf3425889e
zwxzwxzwx21 <alexx0779@gmail.com>
2026-03-06 16:51:42 +0100

    adding more validations

--- solution.py
+++ solution.py
@@ -3,13 +3,26 @@ import re
 
 def add_virtual_column(df: pd.DataFrame, role: str, new_column: str) -> pd.DataFrame:
     operations = ["+", "-", "*"]
-    pattern = "^[a-zA-Z_]*$"
+    pattern = re.compile(r'^[a-zA-Z_]+$')
+    
     # validations
-    for col_label in pd.DataFrame.columns:
-        if bool(re.match(pattern,col_label)) == False:
+    ### Column labels must consist only of letters and underscores (_).
+    role_columns_split = re.split(r'[+-*]', role)
+
+    if not pattern.match(new_column):
+        return pd.DataFrame([])
+    
+    if not all(pattern.match((col) for col in df.columns)):
+        return pd.DataFrame([])
+    
+    for col_label in role_columns_split:
+        col_label = col_label.strip()
+        if col_label and (col_label not in df.columns or not pattern.match(col_label)):
             return pd.DataFrame([])
-    if 
-    return pd.DataFrame([]) 
+
+    ### The function must support basic operations: addition (+), subtraction (-), and multiplication (*).
+    if not any(op in role for op in operations):
+        return pd.DataFrame([]) 
 
 df = pd.DataFrame([[1, 1]] * 5, columns = ["label_one", "label_two"])
 
